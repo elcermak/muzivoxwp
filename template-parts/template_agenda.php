7d7007cad887d5f5a592964ea9fc9818e2c6dbd4
Kamicron <ludovic.chevroulet@outlook.fr>
2023-02-03 14:46:12 +0100

    filtre sur date et region v1

--- template-parts/template_agenda.php
+++ template-parts/template_agenda.php
@@ -5,61 +5,196 @@
  */
 
 get_header(); ?>
-
-<table>
-  <?php
-  $args = array(
-    'post_type' => 'agenda',
-    'posts_per_page' => -1,
-    'meta_key' => 'date',
-    'orderby' => 'meta_value',
-    'order' => 'ASC'
-  );
-
-
-  $artiste_posts = get_posts($args);
-  echo '<pre>';
-  print_r($artiste_posts);
-  echo '</pre>';
-  $i = 0;
-  foreach ($artiste_posts as $post) {
-    // Récupération des données du champ "régions"
-    $id_regions = get_field('region_concert');
-
-    // Récupération des données du champ "artiste"
-    $artiste = get_field('artiste_concert');
-
-    // Récupération des données du champ "ville"
-    $ville = get_field('ville');
-
-    // Récupération des données du champ "date"
-    $date = get_field('date');
-
-
-    // Affichage des données
-    $terms = get_terms(array(
-      'taxonomy' => 'region',
-      'hide_empty' => false,
-    ));
-    $taxo = get_the_terms($post->ID, 'region');
+<nav>
+  <ul>
+    <li><a href="?filtre=artiste">Artiste</a></li>
+    <li><a href="?filtre=date">Date</a></li>
+    <li><a href="?filtre=region">Région</a></li>
+  </ul>
+</nav>
 
 
 
 
-  ?>
-    <tr>
+<?php
 
-      <td><?php echo $artiste[0]->post_title; ?> </td>
-      <td><?php echo $ville; ?></td>
-      <td><?php echo $date; ?></td>
-      <td><?php foreach ($terms as $value) {
+// echo '<pre>';
+// print_r($_GET);
+// echo '</pre>';
+
+if ($_GET['filtre'] == "date") { ?>
+  <table>
+    <?php
+    $args = array(
+      'post_type' => 'agenda',
+      'posts_per_page' => -1,
+      'meta_key' => 'date',
+      'orderby' => 'meta_value',
+      'order' => 'ASC'
+    );
+
+    $artiste_posts = get_posts($args);
+    $i = 0;
+    $previous_month = '';
+    $previous_year = '';
+    foreach ($artiste_posts as $post) {
+
+      // Récupération des données du champ "régions"
+      $id_regions = get_field('region_concert');
+
+      // Récupération des données du champ "artiste"
+      $artiste = get_field('artiste_concert');
+
+      // Récupération des données du champ "ville"
+      $ville = get_field('ville');
+
+      // Récupération des données du champ "date"
+      $date = get_field('date');
+
+      // Récupération des données du champ "date"
+      $is_full = get_field('complet');
+
+      // Affichage des données
+      $terms = get_terms(array(
+        'taxonomy' => 'region',
+        'hide_empty' => false,
+      ));
+      $taxo = get_the_terms($post->ID, 'region');
+
+      $month = date("m", strtotime($date));
+      $year = date("Y", strtotime($date));
+
+      if ($month != $previous_month || $year != $previous_year) { ?>
+        <tr class="title_calendar">
+          <td>
+            <h2><?php dateToString($month);
+                echo " " . $year ?></h2>
+          </td>
+        </tr>
+      <?php
+        $previous_month = $month;
+        $previous_year = $year;
+      } ?>
+      <tr>
+
+        <td><?php echo $artiste[0]->post_title; ?> </td>
+        <td><?php echo $ville; ?></td>
+        <td><?php echo $date; ?></td>
+        <td>
+          <?php foreach ($terms as $value) {
             if ($id_regions == $value->term_id) {
               echo $value->name;
             }
-          } ?></td>
-    </tr>
+          } ?>
+        </td>
+        <td><?php echo ($is_full == 1) ? "Complet" : ""; ?></td> <!-- Si concert complet, alors affiche "Complet" sinon affiche "" -->
+      </tr>
+    <?php
+    }
+    ?>
+  </table>
 
-  <?php
-  }
-  ?>
-</table <?php get_footer();
+
+
+
+
+
+
+
+
+
+<?php
+} else if ($_GET['filtre'] == "region") { ?>
+  <table>
+    <?php
+    $args = array(
+      'post_type' => 'agenda',
+      'posts_per_page' => -1,
+      'meta_key' => 'region_concert',
+      'orderby' => 'meta_value',
+      'order' => 'DSC'
+    );
+
+    $artiste_posts = get_posts($args);
+    $i = 0;
+    $previous_region = '';
+    foreach ($artiste_posts as $post) {
+
+      // Récupération des données du champ "régions"
+      $id_regions = get_field('region_concert');
+
+      // Récupération des données du champ "artiste"
+      $artiste = get_field('artiste_concert');
+
+      // Récupération des données du champ "ville"
+      $ville = get_field('ville');
+
+      // Récupération des données du champ "date"
+      $date = get_field('date');
+
+      // Récupération des données du champ "date"
+      $is_full = get_field('complet');
+
+      // Affichage des données
+      $terms = get_terms(array(
+        'taxonomy' => 'region',
+        'hide_empty' => false,
+      ));
+      $taxo = get_the_terms($post->ID, 'region');
+
+      $month = date("m", strtotime($date));
+      $year = date("Y", strtotime($date));
+      foreach ($terms as $value) {
+        if ($id_regions == $value->term_id) {
+          $region = $value->name;
+        }
+      }
+      // echo "<hr>";
+      // echo "previous ".$previous_region;
+      // echo '<br>';
+      // echo "current " .$region;
+      // echo "<hr>";
+
+
+      if ($region != $previous_region) { ?>
+        <tr class="title_calendar">
+          <td>
+            <h2>
+              <?php
+              foreach ($terms as $value) {
+                if ($id_regions == $value->term_id) {
+                  $region = $value->name;
+                  echo $region;
+                }
+              }
+              ?>
+            </h2>
+          </td>
+        </tr>
+      <?php
+        $previous_region = $region;
+      } ?>
+
+
+
+      <tr>
+
+        <td><?php echo $artiste[0]->post_title; ?> </td>
+        <td><?php echo $ville; ?></td>
+        <td><?php echo $date; ?></td>
+        <td>
+        <td>
+          <?php echo $region; ?>
+        </td>
+        </td>
+        <td><?php echo ($is_full == 1) ? "Complet" : ""; ?></td> <!-- Si concert complet, alors affiche "Complet" sinon affiche "" -->
+      </tr>
+    <?php
+    }
+    ?>
+  </table>
+<?php
+}
+?>
+
+<?php get_footer(); ?>
