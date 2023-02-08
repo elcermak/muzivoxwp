99c774128794db3b159f423b508433da47ca1857
Kamicron <ludovic.chevroulet@outlook.fr>
2023-02-08 09:04:18 +0100

    ajout d'un else dans agenda

--- template-parts/template_agenda.php
+++ template-parts/template_agenda.php
@@ -7,6 +7,7 @@
 get_header(); ?>
 
 <main>
+  
   <div class="container container-flex">
 
     <!-- <nav>
@@ -275,6 +276,91 @@ get_header(); ?>
       </table>
 
     <?php
+    } else { ?>
+      <div class="container-rectangle">
+      <div class="btns-filters">
+        <a href="?filtre=artistes" class="btn-filter">Artistes</a>
+        <a href="?filtre=dates" class="btn-filter">Dates</a>
+        <a href="?filtre=regions" class="btn-filter">Régions</a>
+      </div>
+      <table class="agenda-table">
+        <?php
+        $args = array(
+          'post_type' => 'agenda',
+          'posts_per_page' => -1,
+          'meta_key' => 'date',
+          'orderby' => 'meta_value',
+          'order' => 'ASC'
+        );
+
+        $artiste_posts = get_posts($args);
+        $i = 0;
+        $previous_month = '';
+        $previous_year = '';
+        foreach ($artiste_posts as $post) {
+
+          // Récupération des données du champ "régions"
+          $id_regions = get_field('region_concert');
+
+          // Récupération des données du champ "artiste"
+          $artiste = get_field('artiste_concert');
+
+          // Récupération des données du champ "ville"
+          $ville = get_field('ville');
+
+          // Récupération des données du champ "date"
+          $date = get_field('date');
+
+          // Récupération des données du champ "date"
+          $is_full = get_field('complet');
+          echo $is_full;
+
+          // Affichage des données
+          $terms = get_terms(array(
+            'taxonomy' => 'region',
+            'hide_empty' => false,
+          ));
+          $taxo = get_the_terms($post->ID, 'region');
+
+          $month = date("m", strtotime($date));
+          $year = date("Y", strtotime($date));
+
+          if ($month != $previous_month || $year != $previous_year) { ?>
+            <tr class="title_calendar">
+              <td>
+                <h3 class="description__title" id="name-filter">
+                  <?php
+                  dateToString($month);
+
+                  ?>
+                </h3>
+              </td>
+            </tr>
+          <?php
+            $previous_month = $month;
+            $previous_year = $year;
+          } ?>
+          <tr>
+
+            <td><?php echo $artiste[0]->post_title; ?> </td>
+            <td><?php echo $ville; ?></td>
+            <td><?php echo convert_date_format($date); ?></td>
+            <td>
+              <?php foreach ($terms as $value) {
+                if ($id_regions == $value->term_id) {
+                  echo $value->name;
+                }
+              } ?>
+            </td>
+            <td><?php echo ($is_full == 1) ? "Complet" : ""; ?></td> <!-- Si concert complet, alors affiche "Complet" sinon affiche "" -->
+          </tr>
+        <?php
+        }
+        ?>
+      </table>
+    </div>
+  <?php
+
     }
     ?>
   </div>
