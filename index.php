8cee60eb94b93e35f46937be8f6c2c0af7165d22
Kamicron <ludovic.chevroulet@outlook.fr>
2023-03-04 12:45:30 +0100

    Redirection

--- index.php
+++ index.php
@@ -1,20 +1,5 @@
-<?php 
-  get_header();
-
-  if(have_posts()) :
-    while(have_posts()) : the_post();
-    echo "a des post"; echo '<br>';
-
-      // print_r(get_post_type());
-      get_template_part( 'template-parts/content', get_post_type() );
-
-    endwhile;
-  else :
-    echo "pas de post";
-
-    get_template_part( 'template-parts/content', 'none' );
-
-  endif;
-  
-  get_footer(); 
+<?php
+  header('Location: https://muzivox-test.com/fr/accueil/');
+  exit();
 ?>
+
